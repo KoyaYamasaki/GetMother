18d09a94f37b72066386679a8911b66d54872715
Yamasaki Kouya <koya2170@gmail.com>
2024-01-08 16:35:09 +0900

    mother bot initial commit

---
+++ motherbot/Main.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+import OpenAI.Client
+
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
+import System.Environment (setEnv, getEnv)
+import qualified Data.Text as T
+
+request :: ChatCompletionRequest
+request = ChatCompletionRequest 
+        {
+            chcrModel = ModelId "gpt-3.5-turbo",
+            chcrMessages = 
+            [ ChatMessage {
+                chmContent = Just "Write a hello world program in Haskell",
+                chmRole = "user",
+                chmFunctionCall = Nothing,
+                chmName = Nothing
+            }
+            ],
+            chcrFunctions = Nothing,
+            chcrTemperature = Nothing,
+            chcrTopP = Nothing,
+            chcrN = Nothing,
+            chcrStream = Nothing,
+            chcrStop = Nothing,
+            chcrMaxTokens = Nothing,
+            chcrPresencePenalty = Nothing,
+            chcrFrequencyPenalty = Nothing,
+            chcrLogitBias = Nothing,
+            chcrUser = Nothing
+        }
+
+main :: IO ()
+main = do
+    setEnv "key" ""
+    manager <- newManager tlsManagerSettings
+    apiKey <- T.pack <$> getEnv "key"
+        -- create a openai client that automatically retries up to 4 times on network
+        -- errors
+    let client = makeOpenAIClient apiKey manager 4
+    result <- completeChat client request        
+    case result of
+        Left failure -> print failure
+        Right success -> print $ chrChoices success
---
+++ motherbot/README.md
@@ -0,0 +1,5 @@
+# Mother bot
+- ## To Get Started
+> cabal install --lib cpphs http-client http-client-tls openai-hs-0.3.0.0
+> runhaskell Main.hs
+
