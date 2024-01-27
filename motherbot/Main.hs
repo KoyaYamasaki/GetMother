c3ff9f8421c8902be43dc76609eccc58509a6903
Yamasaki Kouya <koya2170@gmail.com>
2024-01-27 21:09:16 +0900

    update source

--- motherbot/Main.hs
+++ motherbot/Main.hs
@@ -5,26 +5,26 @@ import Network.Wai
 import Network.Wai.Handler.Warp (run)
 
 import Network.HTTP.Types.Header (Header)
-import Network.HTTP.Client as HC
 import Network.HTTP.Types ( status200 )
 import Network.HTTP.Client.Conduit (responseOpen, responseBody)
 import Network.HTTP.Simple (httpSource, parseRequest)
 import Network.HTTP.Conduit
-import Data.Aeson
-import qualified Data.ByteString.Lazy.Char8 as LBS
-import qualified Data.ByteString.Char8 as BS
-import Data.Conduit (runConduit, (.|))
-import Data.Conduit
-import qualified Data.Conduit as C
-import qualified Data.Conduit.List as CL
-import Data.ByteString.Builder
-import Data.ByteString.Builder (byteString)
+    ( parseRequest,
+      Request(method),
+      Response(responseBody),
+      RequestBody(RequestBodyLBS) )
+import Data.Aeson ( encode, decode, object, KeyValue((.=)) )
+import qualified Data.ByteString.Lazy.Char8 as LazyChar8
+import qualified Data.ByteString.Char8 as Char8
+import Data.Conduit (runConduit, (.|), awaitForever)
+import qualified Data.Conduit.List as ConduitList
+import Data.ByteString.Builder (byteString, Builder)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Resource (runResourceT)
-import qualified Data.Conduit.Combinators as CC
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.Text as T
-import qualified Network.HTTP.Client as HC
+import qualified Data.Conduit.Combinators as ConduitCombinators
+import qualified Data.ByteString.Lazy as Lazy
+import qualified Data.Text as Text
+import qualified Network.HTTP.Client as HTTPClient
 import Data.ByteString (toStrict)
 
 type ContentType = String
@@ -32,13 +32,13 @@ type ContentType = String
 streamBody :: Network.HTTP.Conduit.Request -> (Builder -> IO ()) -> IO () -> IO ()
 streamBody req send flush = runResourceT $ do
     runConduit $ httpSource req responseBody
-        -- .| CC.map BL.fromStrict
-        -- .| CC.map createCompletion
-        -- .| CC.map (decode :: BL.ByteString -> Maybe CompletionDict)
-        -- .| CC.concatMap (map BS.pack . T.unpack . content . delta . head . choices)
-        -- .| CC.mapM_ (liftIO . send . byteString)
+        -- .| ConduitCombinators.map BL.fromStrict
+        -- .| ConduitCombinators.map createCompletion
+        -- .| ConduitCombinators.map (decode :: BL.ByteString -> Maybe CompletionDict)
+        -- .| ConduitCombinators.concatMap (map BS.pack . T.unpack . content . delta . head . choices)
+        -- .| ConduitCombinators.mapM_ (liftIO . send . byteString)
         -- .| getMessage (decode :: BL.ByteString -> Maybe Completion)
-        -- .| CC.concatMap (map content . delta)
+        -- .| ConduitCombinators.concatMap (map content . delta)
         .| awaitForever (liftIO . send . byteString)
 
 -- streamingAPIRequest :: Network.HTTP.Conduit.Request -> 
@@ -47,26 +47,28 @@ streamBody req send flush = runResourceT $ do
 --     manager <- newManager tlsManagerSettings
 --     withResponse req manager $ \res -> 
 --         respond $ responseStream status200 [("Content-Type", "application/json")] $ \write flush -> do
---             responseBody res C.$$+- CL.mapM_ (liftIO . write . (<> "\n"))
+--             responseBody res C.$$+- ConduitList.mapM_ (liftIO . write . (<> "\n"))
 --             liftIO flush
 
-createCompletion :: BL.ByteString -> Maybe Completion
+createCompletion :: Lazy.ByteString -> Maybe Completion
 createCompletion x = decode x :: Maybe Completion
 
-getMessage' :: Maybe Completion -> BS.ByteString
+getMessage' :: Maybe Completion -> Char8.ByteString
 getMessage' comp = do
     case comp of
-        Just jsonData -> BS.pack $ T.unpack $ content $ delta $ head $ choices jsonData
-        Nothing -> BS.pack "Failure "
+        Just jsonData -> Char8.pack $ Text.unpack $ content $ delta $ head $ choices jsonData
+        Nothing -> Char8.pack "Failure "
 
 motherResponse request respond = do
 -- リクエストの作成
-    let authorization = BS.pack $ "Bearer " ++ myApiKey
+    let authorization = Char8.pack $ "Bearer " ++ myApiKey
     let request = "https://api.openai.com/v1/chat/completions"
         messages = [
             Data.Aeson.object [
                 "role" .= ("system" :: String),
-                "content" .= ("私のお母さんになりきって答えてください。年金の仕組みを教えて。" :: String)]
+                "content" .= ("私のお母さんになりきって答えてください。" :: String),
+                "role" .= ("user" :: String),
+                "content" .= ("お母さん、年金の仕組みを教えて。" :: String)]
             ]
         requestBody = encode $ Data.Aeson.object [
             "model" .= ("gpt-3.5-turbo" :: String),
@@ -80,10 +82,10 @@ motherResponse request respond = do
     initialRequest <- liftIO $ parseRequest request
     let request' = initialRequest {
         method = "POST",
-        HC.requestBody = RequestBodyLBS requestBody,
-        HC.requestHeaders = headers }
+        HTTPClient.requestBody = RequestBodyLBS requestBody,
+        HTTPClient.requestHeaders = headers }
 
-    respond $ responseStream status200 [("Content-Type", "application/json;charset=UTF-8")] $ streamBody request'
+    respond $ responseStream status200[("Content-Type", "application/json;charset=UTF-8"), ("Access-Control-Allow-Origin", "*")] $ streamBody request'
 
 main :: IO ()
 main = do
@@ -98,7 +100,7 @@ app request respond = do
 
 serveResponse :: FilePath -> ContentType -> (Network.Wai.Response -> IO ResponseReceived) -> IO ResponseReceived
 serveResponse filePath contentType respond = do
-    content <- LBS.readFile filePath
+    content <- LazyChar8.readFile filePath
     let response = responseLBS
             status200
             [("Content-Type", "text/html")]
@@ -106,4 +108,4 @@ serveResponse filePath contentType respond = do
     respond response
 
 -- APIキーの設定
-myApiKey = "XXX"
+myApiKey = "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"
